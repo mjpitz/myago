15a0442dc76109e4ca140367e916b22073172f82
mya <j.mya.pitz@gmail.com>
2023-12-13 14:55:06 -0600

    add new project

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 		{"okit", "go.pitz.tech/okit", "https://code.pitz.tech/mya/okit", "git"},
 		{"units", "go.pitz.tech/units", "https://github.com/mjpitz/units", "git"},
 		{filepath.Join("gorm", "encryption"), "go.pitz.tech/gorm/encryption", "https://github.com/mjpitz/gorm-encryption", "git"},
-		{"lagg", "go.pitz.tech/lagg", "https://github.com/mjpitz/lagg", "git"},
+		{"spdx-to-md", "go.pitz.tech/spdx-to-md", "https://github.com/mjpitz/spdx-to-md", "git"},
 	}
 
 	for _, generate := range generates {
